7a56344b44e58294765f83de74a55d9225aaf66b
mdm <etomymail@gmail.com>
2025-01-02 07:58:21 +0500

    added a flask to separate process

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from api import start
+from gui.trayIcon import qt_start
+from api import flask_start
+from multiprocessing import Process
 
 if __name__ == '__main__':
-    start()
+    process_flask = Process(target=flask_start, daemon=True)
+    process_flask.start()
+
+    qt_start()
+
